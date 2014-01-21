04254e8a8c1ffece4f5463fe85f217ab2881d89a
Lee Campbell <lee@salk.edu>
2014-01-20 20:29:12 -0800

    Build.bat

---
+++ client/build.bat
@@ -0,0 +1,4 @@
+#!/bin/sh
+rm -rf NV-Uploader.app
+cd pyinstaller
+python pyinstaller.py --noconfirm --upx-dir=/usr/local/bin --windowed ../NV-Uploader.py; mv NV-Uploader/dist/NV-Uploader.app ~/Desktop
